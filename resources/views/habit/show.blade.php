a1faa62ad0625acf622d8e17618596a6955a19d4
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-16 23:49:14 +0900

    show.blade記述

--- resources/views/habit/show.blade.php
+++ resources/views/habit/show.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts/app')
 @section('content')
 
-
+<table>
+    <tr>
+      <th>登録日</th>
+      <td>{{ $habit->created_at->format('Y/m/d') }}</td>
+    </tr>
+    <tr>
+      <th>Title</th>
+      <td>{{ $habit->title }}</td>
+    </tr>
+</table>
 
 @endsection
