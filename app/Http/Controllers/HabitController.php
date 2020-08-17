a656d965b8189fbcb4396c20f26edfc3bd053a78
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-17 23:07:00 +0900

    ExecDateモデルのデータを持ってくるようexec_datesプロパティ定義

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -60,7 +60,7 @@ class HabitController extends Controller
     public function show($habit_id)
     {
         $habit = $this->habit->find($habit_id);
-        $exec_dates = Habit::find($habit_id)->exec_dates();
+        $exec_dates = Habit::find($habit_id)->exec_dates;
         return view('/habit/show', compact('habit', 'exec_dates'));
     }
 
