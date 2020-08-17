21bc11ad245910c416e24e291f9d97e0f0a95885
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-17 23:07:44 +0900

    foreachでexec_datesプロパティの中身を表示

--- resources/views/habit/show.blade.php
+++ resources/views/habit/show.blade.php
@@ -10,6 +10,16 @@
       <th>Title</th>
       <td>{{ $habit->title }}</td>
     </tr>
+    @foreach($exec_dates as $exec_date)
+      <tr>
+        <th>実施日</th>
+        <td>{{ $exec_date->created_at->format('Y/m/d') }}</td>
+      </tr>
+      <tr>
+        <th>実施内容</th>
+        <td>{{ $exec_date->contents }}</td>
+      </tr>
+    @endforeach
 </table>
 
 @endsection
