7dc72643b31b298712f2ac7642044bd179b42ff9
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-19 14:40:58 +0900

    Habitモデルにソフトデリート実装

--- app/Models/Habit.php
+++ app/Models/Habit.php
@@ -3,9 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Habit extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     public function executions()
     {
         return $this->hasMany('App\Models\Execution');
