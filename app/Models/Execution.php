eb59f9895c45fde901496c852bdd042b1980bd29
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-19 14:41:49 +0900

    Executionモデルにソフトデリート実装+誤記修正(belongTo -> belongsTo)

--- app/Models/Execution.php
+++ app/Models/Execution.php
@@ -3,12 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Execution extends Model
 {
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
     public function habit()
     {
-        return $this->belongTo('App\Models\Habit');
+        return $this->belongsTo('App\Models\Habit');
     }
 
     protected $fillable = [
