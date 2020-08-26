eb1ec72c463e113cbcf1bcef32d97442294fd8c9
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-26 16:29:34 +0900

    create.bladeのブロック構成を修正(divタグ追加)

--- resources/views/habit/create.blade.php
+++ resources/views/habit/create.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts/app')
 @section('content')
 
-<div>習慣にしたいことを登録しましょう</div>
-{!! Form::open(['route' => 'habit.store']) !!}
-  <div>
-    {!! Form::text('title', '', ['placeholder' => '習慣にしたいことを入力してください']) !!}
-    @if ($errors->has('title'))
-      <span>{{ $errors->first('title') }}</span>
-    @endif
+<div class="habit-container">
+  <h2 class="create-habit_title">習慣にしたいことを入力してください</h2>
+  <div class="margin-top">
+    {!! Form::open(['route' => 'habit.store']) !!}
+      <div>
+        {!! Form::text('title', '', ['placeholder' => '例)ランニング', 'class' => 'input-area']) !!}
+        @if ($errors->has('title'))
+          <span class="error-message">{{ $errors->first('title') }}</span>
+        @endif
+      </div>
+      <div class="margin-top">
+        {!! Form::button('追加', ['type' => 'submit', 'class' => 'btn btn-add']) !!}
+      </div>
+    {!! Form::close() !!}
   </div>
-  <div>
-    {!! Form::submit('追加') !!}
-  </div>
-{!! Form::close() !!}
+</div>
 
 @endsection
