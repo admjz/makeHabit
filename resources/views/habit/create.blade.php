a461808e35f9ef89370f7c2e5260c1df29cfab43
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-27 00:30:36 +0900

    create.bladeクラス名修正

--- resources/views/habit/create.blade.php
+++ resources/views/habit/create.blade.php
@@ -2,16 +2,16 @@
 @section('content')
 
 <div class="habit-container">
-  <h2 class="create-habit_title">習慣にしたいことを入力してください</h2>
-  <div class="margin-top">
+  <h2 class="container_title">習慣にしたいことを入力してください</h2>
+  <div class="margin-top50">
     {!! Form::open(['route' => 'habit.store']) !!}
       <div>
-        {!! Form::text('title', '', ['placeholder' => '例)ランニング', 'class' => 'input-area']) !!}
+        {!! Form::text('title', '', ['placeholder' => '例)筋トレ', 'class' => 'input-area']) !!}
         @if ($errors->has('title'))
           <span class="error-message">{{ $errors->first('title') }}</span>
         @endif
       </div>
-      <div class="margin-top">
+      <div class="margin-top50">
         {!! Form::button('追加', ['type' => 'submit', 'class' => 'btn btn-add']) !!}
       </div>
     {!! Form::close() !!}
