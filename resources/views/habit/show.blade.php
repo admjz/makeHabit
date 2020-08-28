767f229ddf3d90ee14c2934d7fc635d5d46a2bb0
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-28 23:01:22 +0900

    show.bladeブロック構成変更(tableタグ -> divタグへ)

--- resources/views/habit/show.blade.php
+++ resources/views/habit/show.blade.php
@@ -1,48 +1,44 @@
 @extends('layouts/app')
 @section('content')
 
-<table>
-  <tr>
-    <th>開始日</th>
-    <td>{{ $habit->created_at->format('Y/m/d') }}</td>
-  </tr>
-  <tr>
-    <th>タイトル</th>
-    <td>{{ $habit->title }}</td>
-  </tr>
-</table>
-<div>
-  {!! Form::open(['route' => 'execution.store']) !!}
-    {!! Form::hidden('habit_id', $habit->id) !!}
-    {!! Form::text('contents', '', ['placeholder' => 'やった内容を記入できます']) !!}
-    {!! Form::submit('実施しました！') !!}
-  {!! Form::close() !!}
-</div>
-<table>
+<div class="habit-container">
+  <h2 class="container_title">{{ $habit->title }}</h2>
+  <div class="margin-top50">
+    {!! Form::open(['route' => 'execution.store']) !!}
+      {!! Form::hidden('habit_id', $habit->id) !!}
+      <div>
+        {!! Form::text('contents', '', ['class' => 'input-area', 'placeholder' => '例)腹筋50回、腕立て伏せ50回']) !!}
+        @if ($errors->has('contents'))
+          <span class="error-message">{{ $errors->first('contents') }}</span>
+        @endif
+      </div>
+      <div class="margin-top30">
+        {!! Form::button('実施しました！', ['type' => 'submit', 'class' => 'btn btn-add']) !!}
+      </div>
+    {!! Form::close() !!}
+  </div>
   @foreach($executions as $execution)
-    <tr>
-      <th>実施日</th>
-      <td>{{ $execution->created_at->format('Y/m/d') }}</td>
-    </tr>
-    <tr>
-      <th>実施内容</th>
-      <td>{{ $execution->contents }}</td>
-    </tr>
-    <tr>
-      <th></th>
-      <td>
-        <a href="{{ route('execution.edit', $execution->id) }}">やった内容を編集する</a>
-      </td>
-    </tr>
-    <tr>
-      <th></th>
-      <td>
+    <div class="execution-box">
+      <div class="execution-box_inner">
+        <div class="execution-date">{{ $execution->created_at->format('Y/m/d') }}</div>
+        <div class="execution-contents margin-top30">
+          @if (!empty($execution->contents))
+            {{ $execution->contents }}
+          @else
+            &nbsp;
+          @endif
+          </div>
+      </div>
+      <div class="btn edit-form margin-top30">
+        <a href="{{ route('execution.edit', $execution->id) }}"><i class="fas fa-edit edit-execution"></i></a>
+      </div>
+      <div class="delete-form margin-top30">
         {!! Form::open(['route' => ['execution.destroy', $execution->id], 'method' => 'DELETE'])!!}
-          {!! Form::button('削除する', ['type' => 'submit'])!!}
+          {!! Form::button('<i class="fas fa-trash-alt fa-2x"></i>', ['type' => 'submit', 'class' => 'btn delete-btn'])!!}
         {!! Form::close()!!}
-      </td>
-    </tr>
+      </div>
+    </div>
   @endforeach
-</table>
+</div>
 
 @endsection
