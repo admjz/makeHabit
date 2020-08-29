d5e6a19b6bfa3858645ac81aba33b419781a0ed1
Ikuma Sugawara <admjz1411@gmail.com>
2020-08-29 17:24:01 +0900

    edit.bladeブロック構成変更

--- resources/views/habit/edit.blade.php
+++ resources/views/habit/edit.blade.php
@@ -1,17 +1,21 @@
 @extends('layouts/app')
 @section('content')
 
-<div>新しいタイトルを入力してください</div>
-{!! Form::open(['route' => ['habit.update', $habit->id], 'method' => 'PUT']) !!}
-  <div>
-    {!! Form::text('title', $habit->title) !!}
-    @if ($errors->has('title'))
-      <span>{{ $errors->first('title') }}</span>
-    @endif
+<div class="habit-container">
+  <h2 class="container_title">新しいタイトルを入力してください</h2>
+  <div class="margin-top50">
+    {!! Form::open(['route' => ['habit.update', $habit->id], 'method' => 'PUT']) !!}
+      <div>
+        {!! Form::text('title', $habit->title, ['class' => 'input-area']) !!}
+        @if ($errors->has('title'))
+          <span class="error-message">{{ $errors->first('title') }}</span>
+        @endif
+      </div>
+      <div class="margin-top50">
+        {!! Form::button('更新', ['type' => 'submit', 'class' => 'btn btn-add']) !!}
+      </div>
+    {!! Form::close() !!}
   </div>
-  <div>
-    {!! Form::submit('更新する') !!}
-  </div>
-{!! Form::close() !!}
+</div>
 
 @endsection
