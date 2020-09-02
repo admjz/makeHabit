e9000d43bf025a48fda30c8322336b0d742f141d
Ikuma Sugawara <admjz1411@gmail.com>
2020-09-03 00:51:01 +0900

    ページネーションリンク表示用ブロック記述

--- resources/views/habit/show.blade.php
+++ resources/views/habit/show.blade.php
@@ -2,47 +2,52 @@
 @section('content')
 
 <div class="habit-container">
-  <h2 class="container_title">{{ $habit->title }}</h2>
-  <div class="margin-top50">
-    {!! Form::open(['route' => 'execution.store']) !!}
-      {!! Form::hidden('habit_id', $habit->id) !!}
-      <div>
-        {!! Form::textarea('contents', '', ['class' => 'input-area', 'placeholder' => '例)腹筋50回、腕立て伏せ50回 &#13;&#10;&#13;&#10;※何も記入しなくても記録できます', 'rows' => 5]) !!}
-        @if ($errors->has('contents'))
-          <span class="error-message">{{ $errors->first('contents') }}</span>
-        @endif
-      </div>
-      <div class="margin-top30">
-        @if (isset($execDate))
-          {!! Form::button('記録する！', ['type' => 'submit', 'class' => 'btn btn-add']) !!}
-        @else
-          {!! Form::button('今日の分は記録済みです', ['disabled', 'type' => 'submit', 'class' => 'btn btn-add btn-disabled']) !!}
-        @endif
-      </div>
-    {!! Form::close() !!}
-  </div>
-  @foreach($executions as $execution)
-    <div class="execution-box">
-      <div class="execution-box_inner">
-        <div class="execution-date">{{ $execution->created_at->format('Y/m/d') }}</div>
-        <div class="execution-contents margin-top30">
-          @if (!empty($execution->contents))
-            {{ $execution->contents }}
-          @else
-            &nbsp;
+  <div>
+    <h2 class="container_title">{{ $habit->title }}</h2>
+    <div class="margin-top50">
+      {!! Form::open(['route' => 'execution.store']) !!}
+        {!! Form::hidden('habit_id', $habit->id) !!}
+        <div>
+          {!! Form::textarea('contents', '', ['class' => 'input-area', 'placeholder' => '例)腹筋50回、腕立て伏せ50回 &#13;&#10;&#13;&#10;※何も記入しなくても記録できます', 'rows' => 5]) !!}
+          @if ($errors->has('contents'))
+            <span class="error-message">{{ $errors->first('contents') }}</span>
           @endif
-          </div>
-      </div>
-      <div class="btn edit-form margin-top30">
-        <a href="{{ route('execution.edit', $execution->id) }}"><i class="fas fa-edit edit-execution"></i></a>
-      </div>
-      <div class="delete-form margin-top30">
-        {!! Form::open(['route' => ['execution.destroy', $execution->id], 'method' => 'DELETE'])!!}
-          {!! Form::button('<i class="fas fa-trash-alt fa-2x"></i>', ['type' => 'submit', 'class' => 'btn delete-btn'])!!}
-        {!! Form::close()!!}
-      </div>
+        </div>
+        <div class="margin-top30">
+          @if (isset($execDate))
+            {!! Form::button('記録する！', ['type' => 'submit', 'class' => 'btn btn-add']) !!}
+          @else
+            {!! Form::button('今日の分は記録済みです', ['disabled', 'type' => 'submit', 'class' => 'btn btn-add btn-disabled']) !!}
+          @endif
+        </div>
+      {!! Form::close() !!}
     </div>
-  @endforeach
+    @foreach($executions as $execution)
+      <div class="execution-box">
+        <div class="execution-box_inner">
+          <div class="execution-date">{{ $execution->created_at->format('Y/m/d') }}</div>
+          <div class="execution-contents margin-top30">
+            @if (!empty($execution->contents))
+              {{ $execution->contents }}
+            @else
+              &nbsp;
+            @endif
+            </div>
+        </div>
+        <div class="btn edit-form margin-top30">
+          <a href="{{ route('execution.edit', $execution->id) }}"><i class="fas fa-edit edit-execution"></i></a>
+        </div>
+        <div class="delete-form margin-top30">
+          {!! Form::open(['route' => ['execution.destroy', $execution->id], 'method' => 'DELETE'])!!}
+            {!! Form::button('<i class="fas fa-trash-alt fa-2x"></i>', ['type' => 'submit', 'class' => 'btn delete-btn'])!!}
+          {!! Form::close()!!}
+        </div>
+      </div>
+    @endforeach
+  </div>
+  <div class="pager margin-top50">
+    {{ $executions->appends(request()->all())->links() }}
+  </div>
 </div>
 
 @endsection
