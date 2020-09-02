813afbfd59bc58152196e7e29d8fcb0c89ece91f
Ikuma Sugawara <admjz1411@gmail.com>
2020-09-02 23:59:07 +0900

    UserSeederファイル読み込みするよう記述

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             HabitsTableSeeder::class,
             ExecutionsTableSeeder::class,
+            UsersTableSeeder::class
         ]);
     }
 }
