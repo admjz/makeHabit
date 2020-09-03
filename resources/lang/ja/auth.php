8ee9b977d7c53e599d08dc077d52afbd75e30130
Ikuma Sugawara <admjz1411@gmail.com>
2020-09-03 15:39:33 +0900

    ログイン時のバリデーションエラーメッセージを日本語化

---
+++ resources/lang/ja/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'このメールアドレスは登録されていません。',
+    'throttle' => '試行回数が多すぎます。 しばらく時間を空けてから再度お試しください。',
+
+];
